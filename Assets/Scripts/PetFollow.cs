bcd6dc07544d8e58508b0670ade1f2a9c16c41ab
unknown <26633902v@iespoblenou.org>
2019-05-19 14:40:22 +0200

    Pet te sigue

---
+++ Assets/Scripts/PetFollow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PetFollow : MonoBehaviour {
+
+    public float speed;
+    public Animator animator;
+
+    //A quien sigo
+    private Transform target;
+
+	// Use this for initialization
+	void Start () {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Vector2.Distance(transform.position, target.position)>2) {
+            animator.SetFloat("Speed", 1f);
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+        else
+        {
+            animator.SetFloat("Speed", 0f);
+        }
+
+        
+
+	}
+}
