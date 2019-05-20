c0d334ff2eae6746bcfe27c78946ff8e6b2fe5c3
Ricard Villegas Ortiz <21755008k@iespoblenou.org>
2019-05-20 18:46:32 +0200

    Puzzles to wapos

---
+++ Assets/Scripts/Draggable.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Draggable : MonoBehaviour
+{
+
+	private bool selected;
+	public GameObject pieza;
+	public GameObject objetivo;
+	private Vector2 initialPosition;
+
+	void Start()
+	{
+		initialPosition = pieza.transform.position;
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		if (selected)
+		{
+			Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			transform.position = new Vector2(cursorPos.x, cursorPos.y);
+		}
+
+		if (Input.GetMouseButtonUp(0))
+		{
+			drop();
+			selected = false;
+		}
+	}
+
+	private void OnMouseOver()
+	{ 
+		print("ASD");
+		if (Input.GetMouseButtonDown(0))
+		{
+			selected = true;
+		}
+	}
+	
+	public void drop()
+	{
+		if (pieza.tag.Equals(objetivo.tag))
+		{
+			float distancia = Vector2.Distance(pieza.transform.position, objetivo.transform.position);
+			print("distancia" + distancia);
+			if (distancia < 1)
+			{
+				pieza.transform.position = objetivo.transform.position;
+				print("ENCAJA");
+			}
+			else
+			{
+				pieza.transform.position = initialPosition;
+			}
+		}
+		else
+		{
+			pieza.transform.position = initialPosition;
+		}
+	}
+}
