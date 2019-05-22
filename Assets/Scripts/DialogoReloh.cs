61b76f94dc6d4c8b4c4639bf975eaa2082e8e170
Ricard Villegas Ortiz <21755008k@iespoblenou.org>
2019-05-22 17:49:46 +0200

    hola

---
+++ Assets/Scripts/DialogoReloh.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogoReloh : MonoBehaviour {
+
+	public NpcTalk[] npcTalk = new NpcTalk[2];
+
+	private bool dialogoConcluido = false;
+
+	internal ControladorDialogoReloj controlDialogoR;
+
+	public EslaHora reloj;
+
+	// Use this for initialization
+	void Start () {
+
+		controlDialogoR = FindObjectOfType<ControladorDialogoReloj> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.CompareTag("Player"))
+		{
+			if (!reloj.bien)
+			{
+			other.GetComponent<PlayerInput> ().speed = 0;
+
+			
+
+				if (!dialogoConcluido)
+				{
+					controlDialogoR.ProximoDialogo(npcTalk[0]);
+				}
+				else
+				{
+					controlDialogoR.ProximoDialogo(npcTalk[1]);
+				}
+
+				dialogoConcluido = true;
+			}
+		}
+	}
+}
